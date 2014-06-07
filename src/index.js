029e7bf3a2745b076afd70a96f73e9c226b6cb91
Brian Di Palma <offler@gmail.com>
2014-06-07 14:34:54 +0100

    Add function `createStoreAndActions` to the `es6-flux` package.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
+import {Dispatcher} from './Dispatcher';
+
+export function createStoreAndActions(storeConstructor, actionsConstructor) {
+	var dispatcher = new Dispatcher();
+	var store = new storeConstructor();
+	var actions = new actionsConstructor(dispatcher);
+
+	dispatcher.register((payload) => store.handleAction(payload));
+
+	return [store, actions];
+}
+
 export {Store} from "./Store";
 export {Dispatcher} from "./Dispatcher";
